e5a87a00f6519b5925784eaca2b4226d25ac069c
trankhacnhu <23730206@ms.uit.edu.vn>
2026-03-25 13:13:41 +0700

    update source

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>HELLO WORLD</h1>
+    </div>
+  )
+}
+
+export default App
